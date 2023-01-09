6ad822744aaaa76cc029c83672183c9a4a3a9cfe
zkrzn <15352294+zkrzn@users.noreply.github.com>
2023-01-10 00:42:03 +0100

    Update upload.js

--- pages/upload.js
+++ pages/upload.js
@@ -1,46 +1,61 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import Dropzone from 'react-dropzone'
+import { useRef, useState } from "react";
 
-const VideoUpload = () => {
-  const [uploadedFile, setUploadedFile] = useState(null)
-  const [uploadedFileUrl, setUploadedFileUrl] = useState('')
-  const [uploadError, setUploadError] = useState(null)
+export default function VideoInput({ width, height }) {
+  const inputRef = useRef();
+  const [source, setSource] = useState();
 
-  const onDrop = async (acceptedFiles) => {
-    try {
-      const formData = new FormData()
-      formData.append('video', acceptedFiles[0])
-      const res = await axios.post('/api/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      setUploadedFile(res.data.file)
-      setUploadedFileUrl(res.data.url)
-    } catch (err) {
-      setUploadError(err.response.data.error)
-    }
-  }
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    setSource(url);
+  };
+
+  const handleChoose = (event) => {
+    inputRef.current.click();
+  };
 
   return (
-    <div>
-      <Dropzone onDrop={onDrop} accept="video/*">
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop a video file here, or click to select a file</p>
-          </div>
+    <>
+    <h1 className="text-black text-center text-8xl m-10"> Video Uploader </h1>
+    <div className="relative rounded-lg overflow-hidden shadow-lg bg-gray-100 h-full">
+      <input
+        ref={inputRef}
+        className="hidden"
+        type="file"
+        onChange={handleFileChange}
+        accept=".mov,.mp4"
+      />
+      <div className="flex h-full items-center justify-center">
+        {source && (
+          <video
+            className="block w-fit h-96  object-cover"
+            src={source}
+            controls
+          />
         )}
-      </Dropzone>
-      {uploadedFile && (
-        <video src={uploadedFileUrl} controls />
-      )}
-      {uploadError && (
-        <p>{uploadError}</p>
-      )}
-    </div>
-  )
-}
+        {!source && (
+          <button
+            className="mt-4 mr-4 bg-red-600 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+            onClick={handleChoose}
+          >
+            Choisir une vidéo
+          </button>
+        )}
+      </div>
+      
+      {source && (
+      <div className="p-4 font-bold text-center text-gray-600">
+      <button
+            className="mt-4 mr-4 bg-red-600 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+            onClick={handleChoose}>
+            Choisir une vidéo
+          </button> 
+          </div>
 
-export default VideoUpload
+        )}
+        
+
+    </div>
+    </>
+  );
+}
